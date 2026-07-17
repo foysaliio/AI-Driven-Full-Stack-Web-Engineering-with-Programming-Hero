d75fb04855d9f1868eef7fbc5778e41eb5f04663
Foysal Hossien <foysalhossien77@gmail.com>
2026-07-18 02:42:06 +0600

    feat(js): determine bmi category using if else if

---
+++ Java Script/Javascript Practice Task/Task 21 - BMI Category/app.js
@@ -0,0 +1,11 @@
+const bmi = 29;
+
+if (bmi < 18.5) {
+  console.log("Underweight");
+} else if (bmi <= 24.9) {
+  console.log("Normal weight");
+} else if (bmi <= 29.9) {
+  console.log("Overweight");
+} else {
+  console.log("Obese");
+}

f11b686ef3607ae427561fe9c7b96120ad98dd7b
Foysal Hossien <foysalhossien77@gmail.com>
2026-07-19 23:58:58 +0600

    feat(js): compare for while and do while loops

---
+++ Java Script/Javascript Practice Task/Task 44 - Comparing Loop Types/app.js
@@ -0,0 +1,16 @@
+for (let i = 1; i <= 5; i++) {
+  console.log(`For loop ${i}`);
+}
+
+let j = 1;
+while (j <= 5) {
+  console.log(`While loop ${j}`);
+  j++;
+}
+
+let k = 1;
+
+do {
+  console.log(`Do While loop ${k}`);
+  k++;
+} while (k <= 5);

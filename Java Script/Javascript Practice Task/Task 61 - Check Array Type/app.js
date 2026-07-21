234177cd742bfeba5d44023b5885806ab3acf332
Foysal Hossien <foysalhossien77@gmail.com>
2026-07-21 20:25:04 +0600

    feat(js): check whether values are arrays

---
+++ Java Script/Javascript Practice Task/Task 61 - Check Array Type/app.js
@@ -0,0 +1,8 @@
+const favNumber = [7, 77, 777];
+const favName = "Anamarzia";
+
+const favNumberIsArray = Array.isArray(favNumber);
+const favNameIsArray = Array.isArray(favName);
+
+console.log(favNumberIsArray);
+console.log(favNameIsArray);

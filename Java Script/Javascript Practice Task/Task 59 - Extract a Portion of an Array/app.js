178b6ee8da1ca12fa679a684051cb57791c17c8d
Foysal Hossien <foysalhossien77@gmail.com>
2026-07-21 20:16:33 +0600

    feat(js): extract a portion of an array

---
+++ Java Script/Javascript Practice Task/Task 59 - Extract a Portion of an Array/app.js
@@ -0,0 +1,6 @@
+const favoritePlayers = ["Backham", "Neymar", "Kaka", "Root", "Boult"];
+
+const extraFavoritePlayers = favoritePlayers.slice(1, 4);
+
+console.log(favoritePlayers);
+console.log(extraFavoritePlayers);

9d613df07f8e0c9f515c76c5a6547e8a2bd9b948
Foysal Hossien <foysalhossien77@gmail.com>
2026-07-23 03:19:01 +0600

    feat(js): practice string slicing

---
+++ Java Script/Javascript Practice Task/Task 75 - String Slicing/app.js
@@ -0,0 +1,7 @@
+let sentence = "Learning JavaScript is fun!";
+
+const getFirstWord = sentence.slice(0, 8);
+const getLastWord = sentence.slice(-4);
+
+console.log(getFirstWord);
+console.log(getLastWord);

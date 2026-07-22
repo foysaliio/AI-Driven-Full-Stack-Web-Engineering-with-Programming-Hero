72f763b8880850dffc4568a0e19794b268825ec3
Foysal Hossien <foysalhossien77@gmail.com>
2026-07-23 03:14:09 +0600

    feat(js): practice string case conversion

---
+++ Java Script/Javascript Practice Task/Task 74 - String Case Conversion/app.js
@@ -0,0 +1,9 @@
+const ProgrammingLang = "   JaVaScRiPt   ";
+
+const progLangUpper = ProgrammingLang.toUpperCase();
+const progLangLower = ProgrammingLang.toLowerCase();
+const progLangTrimmed = ProgrammingLang.trim();
+
+console.log(progLangUpper);
+console.log(progLangLower);
+console.log(progLangTrimmed);

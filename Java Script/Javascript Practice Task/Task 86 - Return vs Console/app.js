ddd1906d01e441692697b718302d62d1bb2c3d7d
Foysal Hossien <foysalhossien77@gmail.com>
2026-07-24 00:05:50 +0600

    feat(js): practice function return values

---
+++ Java Script/Javascript Practice Task/Task 86 - Return vs Console/app.js
@@ -0,0 +1,11 @@
+function squareNumber(number) {
+  return number * number;
+}
+
+const result1 = squareNumber(32);
+const result2 = squareNumber(14);
+const result3 = squareNumber(83);
+
+console.log(result1);
+console.log(result2);
+console.log(result3);
